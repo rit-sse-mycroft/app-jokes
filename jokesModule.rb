8ec86b194fcf330d61155f42ca318fc10edfe2a9
Jesse Jurman <j.r.jurman@gmail.com>
2014-01-16 11:15:36 -0500

    added special case and turned symbols to strings

--- jokesModule.rb
+++ jokesModule.rb
@@ -2,15 +2,19 @@ module JokeModule
   extend self
 
   def knock_knock(joke)
-    [[:tts, "Knock Knock"], [:stt, "Who's there"], [:tts, joke["set_up"]], [:stt, "#{joke["set_up"]} who"], [:tts, joke["punchline"]]]
+    [['tts', "Knock Knock"], ['delay', joke["setup_delay"] or 2], ['tts', joke["set_up"]], ['delay', joke["punchline_delay"] or 2], ['tts', joke["punchline"]]]
   end
 
   def one_liner(joke)
-    [[:tts, joke]]
+    [['tts', joke]]
   end
 
   def normal(joke)
-    [[:tts, joke["set_up"]], [:delay, joke["delay"] or 2], [:tts, joke["punchline"]]]
+    [['tts', joke["set_up"]], ['delay', joke["delay"] or 2], ['tts', joke["punchline"]]]
+  end
+
+  def special(joke)
+    joke
   end
 
 end
