dcf836f854b92284301571618e5dad7f76cb58c5
Jesse Jurman <j.r.jurman@gmail.com>
2014-01-16 10:54:46 -0500

    added jokesModule.rb

---
+++ jokesModule.rb
@@ -0,0 +1,16 @@
+module JokeModule 
+  extend self
+
+  def knock_knock(joke)
+    [[:tts, "Knock Knock"], [:stt, "Who's there"], [:tts, joke["set_up"]], [:stt, "#{joke["set_up"]} who"], [:tts, joke["punchline"]]]
+  end
+
+  def one_liner(joke)
+    [[:tts, joke]]
+  end
+
+  def normal(joke)
+    [[:tts, joke["set_up"]], [:delay, 2], [:tts, joke["punchline"]]]
+  end
+
+end
