cb4cc60ad21ec3223a470bc05b87f97a1f083a7d
kristenmills <kristen@kristen-mills.com>
2014-01-24 17:57:15 -0500

    Updated grammar

---
+++ grammar.rb
@@ -0,0 +1,13 @@
+﻿require 'srgs'
+
+module WeatherGrammar
+  include Srgs::DSL
+
+  extend self
+
+  grammar 'joke' do
+    public_rule 'joke' do
+      item "Mycroft tell me a joke"
+    end
+  end
+end
