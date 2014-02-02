92a0be2b4a4112cbe5f7821119d343f47cbfa694
kristenmills <kristen@kristen-mills.com>
2014-02-01 22:15:00 -0500

    Updated to use new ruby mycroft gem

--- jokes.rb
+++ jokes.rb
@@ -14,43 +14,34 @@ class Jokes < Mycroft::Client
     @jokes = YAML.load_file('./jokes.yml').shuffle
     @jokes_used = Array.new
     @dependencies = {}
-    @state = "APP_DOWN"
     @sent_grammar = false
     super
   end
 
-  def connect
-    # Your code here
-  end
-
-  def on_data(parsed)
-    if parsed[:type] == 'MSG_BROADCAST'
-      if parsed[:data]["content"]["text"].include? 'joke'
-        tell_joke
+  on 'APP_DEPENDENCY' do |data|
+    update_dependencies(data)
+    puts "Current status of dependencies"
+    puts @dependencies
+    if not @dependencies['stt'].nil?
+      if @dependencies['stt']['stt1'] == 'up' and not @sent_grammar
+        up
+        data = {grammar: { name: 'joke', xml: File.read('./grammar.xml')}}
+        query('stt', 'load_grammar', data)
+        @sent_grammar = true
+      elsif @dependencies['stt']['stt1'] == 'down' and @sent_grammar
+        @sent_grammar = false
+        down
       end
-    elsif parsed[:type] == 'APP_DEPENDENCY'
-      # Dependencies: Speech to Text, Text to Speech
-      # Look for Speech
-      update_dependencies(parsed[:data])
-      puts "Current status of dependencies"
-      puts @dependencies
-      if not @dependencies['stt'].nil?
-        if @dependencies['stt']['stt1'] == 'up' and not @sent_grammar
-          up
-          data = {grammar: { name: 'joke', xml: File.read('./grammar.xml')}}
-          query('stt', 'load_grammar', data)
-          @sent_grammar = true
-        elsif @dependencies['stt']['stt1'] == 'down' and @sent_grammar
-          @sent_grammar = false
-          down
-        end
-      end
-    elsif parsed[:type] == 'MSG_GENERAL_FAILURE'
-      puts parsed[:data]['message']
     end
   end
 
-  def on_end
+  on 'MSG_BROADCAST' do |data|
+    if data["content"]["text"].include? 'joke'
+      tell_joke
+    end
+  end
+
+  on 'end' do
     query('stt', 'unload_grammar', {grammar: 'joke'})
   end
 
